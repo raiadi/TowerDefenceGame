69dd53791431e49d7d0446540e997f1b55c738c0
raiadi <thulung.a@hotmail.com>
2018-08-21 22:58:30 +0100

    second commit

--- Game.cs
+++ Game.cs
@@ -2,9 +2,12 @@ namespace TowerDefence
 {
   class Tower
   {
-    public void Main()
+    public static void Main()
     {
-      Tower tower = new Tower();
+      Map map = new Map(8,5);
+
+
+      int area = map.Width * map.Height;
     }
   }
 }
--- Map.cs
+++ Map.cs
@@ -2,6 +2,12 @@ namespace TowerDefence
 {
   class Map
   {
+    public readonly int Width;
+    public readonly int Height;
 
+    public Map (int width, int height){
+      Width = width;
+      Height = height;
+    }
   }
 }
